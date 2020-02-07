c5d4a3289b0b04f57d61ef92f2dd24cfb390b59d
Juan Jimenez <jimenez.f.juan.m@gmail.com>
2020-02-07 12:38:39 -0400

    Initialize repo

---
+++ src/api.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const serverless = require("serverless-http");
+
+const app = express();
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.json({
+    hello: "Hi!"
+  });
+});
+
+router.get("/test", (req, res) => {
+  res.json({
+    hello: "Test!"
+  });
+});
+
+app.use("/.netlify/functions/api", router);
+
+module.exports.handler = serverless(app);
